84bcfe65a32a84299aedeef17a5300630b71dade
DmitryO <breakdance101@yandex.ru>
2018-02-03 19:28:16 +0700

    03.02 - Fetching Data on Route Change

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import { Provider, connect } from 'react-redux';
 import { toggleTodo } from '../actions'
 import TodoList from './TodoList';
 import { todos } from '../reducers';
 import { getVisibleTodos } from '../reducers';
+import { fetchTodos } from '../api';
 
-const mapStateToProps = (state, { match:{params} } ) => ({
-	todos: getVisibleTodos(state, params.filter || 'all')
-});
+class VisibleTodoList extends Component {
+
+	//после отображения компонента выводим в консоль фетч
+	componentDidMount(){
+		fetchTodos(this.props.filter).then(todos =>
+			console.log(this.props.filter, todos)
+		)
+	}
+
+	//после обновления компонента (смены фильтра у роутера) выводим в консоль фетч
+	componentDidUpdate(prevProps){
+		if (this.props.filter !== prevProps.filter){
+			fetchTodos(this.props.filter).then(todos=>
+				console.log(this.props.filter, todos)
+			)
+		}
+	}
+
+	render(){
+		return <TodoList {...this.props} />;
+	}
+
+}
+
+// наделим компонент пропсом фильтра - this.props.filter
+const mapStateToProps = (state, { match:{params} } ) => {
+	const filter = params.filter || 'all';
+	return {
+		todos: getVisibleTodos(state, filter),
+		filter
+	}
+}
 
 // const mapDispatchToProps = (dispatch) => ({
 // 	onTodoClick(id) { dispatch(toggleTodo(id)) }
 // });
 
-const VisibleTodoList = withRouter(connect(mapStateToProps, { onTodoClick: toggleTodo })(TodoList));
+//оборачиваем компонент withRouter'ом чтобы он мог принимать params.filter
+VisibleTodoList = withRouter(connect(mapStateToProps, { onTodoClick: toggleTodo })(VisibleTodoList));
 
 export default VisibleTodoList;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import configureStore from './configureStore';
 import Root from './components/Root';
-import { fetchTodos } from './api';
-
-// запрашиваем наш объект с аругментом фильтра через задержку с промисом и выводим его в консоль
-fetchTodos('all').then(todos => console.log(todos))
 
 // создаём стор из редьюсера, таже внутри происходит подписка на изменения стора и перерэндер
 const store = configureStore();
