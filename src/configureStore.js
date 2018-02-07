8f7672c455465e524ea798fd506c7bfc0dd942be
DmitryO <breakdance101@yandex.ru>
2018-02-07 15:36:17 +0700

    07.02 - Use libraries redux-promise & redux-logger in middlewares

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,49 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-// с помощью этой функции мы логгируем наш store.dispatch
-const logger = (store) => (next) => {
-	if (!console.group){ return next; }
-	return (action) => {
-		console.group(action.type);
-		console.log('%c prev state', 'color: gray', store.getState());
-		console.log('%c action', 'color: blue',action);
-		const returnValue = next(action);
-		console.log('%c next state', 'color: green', store.getState());
-		console.groupEnd(action.type);
-		return returnValue;
-	};
-};
-
-// с помощью этой функции мы определяем поведение для "обычных" экшенов и экшенов с промисами
-const promise = (store) => (next) => (action) => {
-	// если в качестве action нам пришел promise а не объект вида {type"...",..}
-	if (typeof action.then === 'function') {
-	// мы ожидаем ответа промиса и помещаем его в next( функцию логгирования - logger)
-		return action.then(next);
-	}
-	// иначе если не промис то сразу логгируем
-	return next(action);
-};
-
-// мы создаем обертку над "ориганальным" объектом store.dispatch. после срабатывания экшена:
-// проходим через функцию promise, затем через logger
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-	middlewares.slice().reverse().forEach(middleware => 
-		store.dispatch = middleware(store)(store.dispatch)
-	)
-};
-
+// используем готовые библиотеки для поддержки промисов и логгирования диспатчей
 const configureStore = () => {
 
-	const store = createStore(todoApp);
-	
 	const middlewares = [promise];
 
-	if(process.env.NODE_ENV !== 'production'){ middlewares.push(logger); }
-	wrapDispatchWithMiddlewares(store, middlewares);
-
-	return store;
+	if(process.env.NODE_ENV !== 'production'){ 
+		middlewares.push(createLogger); 
+	}
+	// при необходимости можно добавить persistedState
+	return createStore(
+		todoApp, 
+		applyMiddleware(...middlewares)
+	);
 };
 
 export default configureStore;
